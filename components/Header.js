47a9a01dbe956728faf487cea6e293df375d173e
nghilt7 <nghilt2307@gmail.com>
2022-06-20 08:54:16 +0700

    styles header section

--- components/Header.js
+++ components/Header.js
@@ -9,14 +9,20 @@ import {
 
 export default function Header() {
   return (
-    <div>
-      <div className="">
+    <div className="bg-gray-700 text-gray-200 flex flex-col items-center p-6 select-none sm:flex-row justify-between">
+      <div className="flex">
         <HeaderIcon Icon={HomeIcon} title="HOME" />
         <HeaderIcon Icon={UserIcon} title="ACCOUNT" />
         <HeaderIcon Icon={PhoneIcon} title="CONTACT" />
         <HeaderIcon Icon={InformationCircleIcon} title="ABOUT" />
       </div>
-      <Image width={150} height={100} src="/logo.png" alt="Logo" />
+      <Image
+        className="cursor-pointer active:brightness-110"
+        width={150}
+        height={100}
+        src="/logo.png"
+        alt="Logo"
+      />
     </div>
   );
 }
